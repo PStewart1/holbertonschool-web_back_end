2892662c736eec8ce462ba1cbf852ce241cd5a71
PStewart1 <4920@holbertonstudents.com>
2023-01-06 18:42:31 -0600

    Rewrote to use ES6 for...of operator, while ignoring no-param-reassign

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-param-reassign */
+export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
+  for (const idx of array) {
+    array[i] = appendString + idx;
+    i += 1;
+  }
+
+  return array;
+}
