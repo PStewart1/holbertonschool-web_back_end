13c21e332a4ea3990ab440e0cdd7397d4b57e12d
PStewart1 <4920@holbertonstudents.com>
2023-01-11 15:01:26 -0600

    implemented class Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-underscore-dangle */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw TypeError('Amount must be a number');
+    } else {
+      this._amount = amount;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw TypeError('Currency must be an instance of class Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
