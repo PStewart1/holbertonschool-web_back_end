9345780d1d581950b5e86fbb414c6c93224d8843
PStewart1 <4920@holbertonstudents.com>
2023-01-14 19:07:18 -0600

    returns a DataView of an ArrayBuffer with an Int8Array

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    return Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const array = new Int8Array(buffer, 0, length);
+  array[position] = value;
+  return new DataView(buffer, array);
+}
