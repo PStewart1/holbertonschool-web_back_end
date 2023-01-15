0c2c39fec475cee3769dd939193437845ff8a609
PStewart1 <4920@holbertonstudents.com>
2023-01-14 19:08:53 -0600

    'throw' instead of 'return'

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
   if (position >= length) {
-    return Error('Position outside range');
+    throw Error('Position outside range');
   }
   const buffer = new ArrayBuffer(length);
   const array = new Int8Array(buffer, 0, length);
