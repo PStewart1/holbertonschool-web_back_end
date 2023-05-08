bc442599c327856321a0fd9e8e388d0ac395e0fb
PStewart1 <4920@holbertonstudents.com>
2023-05-08 15:11:45 -0500

    changed Iterator to Iterable

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Let's duck type an iterable object """
-from typing import Iterator, Sequence, List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: Iterator[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """takes a Iterable list 'lst' as argument,
     and returns a list of tuples, containing a sequence and int.
     """
