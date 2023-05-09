ff161910964e71088d545771c8e746fe54ee0d9a
PStewart1 <4920@holbertonstudents.com>
2023-05-09 15:32:33 -0500

    corrected return type to Generator[float]

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,13 +2,13 @@
 """Async Generator"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float]:
     """Loops 10 times, each time asynchronously waits 1 second,
     then yields a random number between 0 and 10 each time.
     """
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-    
