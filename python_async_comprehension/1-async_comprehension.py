e85cea2bf1b18d223e580e87dd55e52ce3ca971c
PStewart1 <4920@holbertonstudents.com>
2023-05-09 15:41:21 -0500

    corrected return type to List[float]

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
 import asyncio
-import random
-from typing import Generator
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """Collects 10 random numbers using an async comprehensing
     over 'async_generator', then returns the 10 random numbers.
     """
