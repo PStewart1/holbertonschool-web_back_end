ecd59ad850be7f1c2bc207e04b40db3fcecd6098
PStewart1 <4920@holbertonstudents.com>
2023-05-09 15:29:28 -0500

    Async Generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Async Generator"""
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """Loops 10 times, each time asynchronously waits 1 second,
+    then yields a random number between 0 and 10 each time.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
+    
---
+++ python_async_comprehension/README.md
@@ -0,0 +1,10 @@
+# Python - Async Comprehension
+
+### Learning Objectives:
+*    How to write an asynchronous generator
+*    How to use async comprehensions
+*    How to type-annotate generators
+
+--- 
+## Author 
+#### Paul S.
