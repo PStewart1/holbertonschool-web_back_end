9bc31722b68e4c47bf0cfa1f30ffe5bc4ce7aa84
PStewart1 <4920@holbertonstudents.com>
2023-05-17 17:42:21 -0500

    Hypermedia pagination

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+""" Hypermedia pagination """
+import csv
+import math
+from typing import List, Dict, Any, Union
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Get page from dataset
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """ returns a dictionary containing the necessary information
+            of the dataset for pagination.
+        """
+        info = {
+            "page_size": page_size,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": page + 1 if self.get_page(page, page_size) else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": math.ceil(len(self.dataset()) / page_size),
+        }
+
+        return info
