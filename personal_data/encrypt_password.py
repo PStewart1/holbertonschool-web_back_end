8e39603308b812e5bc9bb1a818d811af5d3a1aba
PStewart1 <4920@holbertonstudents.com>
2023-05-19 16:25:19 -0500

    changed return type to bytes

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -3,6 +3,6 @@
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """ returns a salted, hashed password, which is a byte string """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
