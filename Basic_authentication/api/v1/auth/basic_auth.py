39e1676748551ebe22340541799b12595fd9f875
PStewart1 <4920@holbertonstudents.com>
2023-05-24 20:10:49 -0500

    Basic auth

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+module to manage the baisc API authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class that inherits from Auth
+    """
+    pass
