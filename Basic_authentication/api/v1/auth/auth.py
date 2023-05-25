852337c361bf511cfaecf2d9222f0e3b076a511d
PStewart1 <4920@holbertonstudents.com>
2023-05-24 19:32:47 -0500

    Auth class

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+module to manage the API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """"
+    class to manage the API authentication
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        public method that returns False
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+        public method that returns None
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        public method that returns None
+        """
+        return None
