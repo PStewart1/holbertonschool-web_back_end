d51350376b63ffa55eafa00fee4b71f3a96ca1dc
PStewart1 <4920@holbertonstudents.com>
2023-05-26 18:26:21 -0500

    Session cookie

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@ module to manage the API authentication
 """
 from flask import request
 from typing import List, TypeVar
+import os
 
 
 class Auth():
@@ -35,3 +36,12 @@ class Auth():
         public method that returns None
         """
         return None
+    
+    def session_cookie(self, request=None):
+        """
+        returns a cookie value from a request
+        """
+        if request is None:
+            return None
+        cookie_name = os.getenv("SESSION_NAME")
+        return request.cookies.get(cookie_name)
