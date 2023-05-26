8eefdf6aa148ff2bf61be68b38229830ba542029
PStewart1 <4920@holbertonstudents.com>
2023-05-26 14:49:06 -0500

    Empty session

---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+module to manage the session API authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth class that inherits from Auth
+    """
+    pass
