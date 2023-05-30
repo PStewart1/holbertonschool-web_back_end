547cec78fe753d9d0c33583ea77c367dc912ecef
PStewart1 <4920@holbertonstudents.com>
2023-05-30 16:19:12 -0500

    Log in

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 Flask app module
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -23,6 +23,18 @@ def users():
         return jsonify(email=new_user.email, message='user created')
     except ValueError:
         return jsonify(message='email already registered'), 400
+
+
+@app.route("/sessions", methods=['POST'], strict_slashes=False)
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    response = jsonify(email=email, message='logged in')
+    response.set_cookie('session_id', session_id)
+    return response
 
 
 if __name__ == "__main__":
