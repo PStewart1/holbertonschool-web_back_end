7c891f2f5c4a54a4d65ce87815a569da48d14d4f
PStewart1 <4920@holbertonstudents.com>
2023-06-07 23:53:22 -0500

    Mock logging in

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 """Force locale with URL parameter
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
-from os import getenv
-from requests import request
 
 app = Flask(__name__)
 babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config(object):
@@ -25,21 +29,43 @@ app.config.from_object(Config)
 def index():
     """ GET /
     Return:
-      - 0-index.html
+      - 4-index.html
     """
-    return render_template("0-index.html")
+    return render_template("5-index.html")
 
 
 @babel.localeselector
 def get_locale():
     """ Determine best match for supported languages
     """
-    if request.args.get("locale"):
-        return request.args.get("locale")
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    locale = request.args.get("locale")
+    if locale and locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+def get_user():
+    """ Returns a user dictionary or None if ID cannot be found
+    """
+    user_id = request.args.get('login_as')
+    if user_id:
+        return users.get(int(user_id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """ Finds user and sets it as a global on flask.g.user
+    """
+    login_as = request.args.get("login_as")
+    user = get_user(login_as)
+    if user:
+        from flask import g
+        g.user = user
+
+
+# @babel.timezoneselector
 
 
 if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run()
