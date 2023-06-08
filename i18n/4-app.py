c48e9460c9d28971420d46fba92b96eeb3987191
PStewart1 <4920@holbertonstudents.com>
2023-06-07 23:40:15 -0500

    wrong request

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Force locale with URL parameter
 """
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 from os import getenv
-from requests import request
 
 app = Flask(__name__)
 babel = Babel(app)
