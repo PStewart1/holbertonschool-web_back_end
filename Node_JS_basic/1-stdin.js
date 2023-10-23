82c1ffe4f8f871fa0ea1b3fb3f30ee1706d7bc9d
PStewart1 <4920@holbertonstudents.com>
2023-10-23 11:36:08 -0500

    Switched to different function for reading input

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,14 @@
 // Using Process stdin
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  readline.close();
-});
-
-process.on('exit', () => {
-  console.log('This important software is now closing');
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
