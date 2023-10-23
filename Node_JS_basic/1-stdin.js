641463559b6b93cbaa16b6acfcc2f2b47ed7fe41
PStewart1 <4920@holbertonstudents.com>
2023-10-23 10:40:25 -0500

    Executing basic javascript with Node JS

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+// Execute: node 1-main.js
+// Output:
+// Welcome to Holberton School, what is your name?
+// Your name is: Holberton School
+// $
+
+console.log(str);
+
+  
+  module.exports = displayMessage;
