fa38b5171b3279077e575ebb6ec7dd772eae757b
PStewart1 <4920@holbertonstudents.com>
2023-10-31 21:41:11 -0500

    Hooks

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,26 @@
+// Hooks are useful functions that can be called before execute one or all tests in a suite
+
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let spy;
+  beforeEach(() => {
+    spy = sinon.spy(console, 'log');
+  });
+  afterEach(() => {
+    spy.restore();
+  });
+
+  it('should return 120', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(spy.calledWith('The total is: 120')).to.be.true;
+    expect(spy.calledOnce).to.be.true;
+  });
+  it('should return 20', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(spy.calledWith('The total is: 20')).to.be.true;
+    expect(spy.calledOnce).to.be.true;
+  });
+});
